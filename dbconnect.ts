2b4fd6014214833182c4c4cd28f3a0f4573ddf08
athics <161621769+athicsmsu@users.noreply.github.com>
2025-10-06 02:23:34 +0700

    Update dbconnect.ts

--- dbconnect.ts
+++ dbconnect.ts
@@ -4,8 +4,8 @@ import util from "util";
 export const conn = mysql.createPool({
 	connectionLimit:10,
 	host:"202.28.34.197",
-	user:"web66_65011212117",
-	password: "65011212117@csmsu",
-	database: "web66_65011212117",
+	user:"cp_65011212145",
+	password: "65011212145@csmsu",
+	database: "cp_65011212145",
 });
 export const queryAsync = util.promisify(conn.query).bind(conn);
